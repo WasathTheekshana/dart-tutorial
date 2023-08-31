5076949f2422536fbac89cb3b8418511f4202440
WasathTheekshana <wasath.vt@gmail.com>
2023-08-31 18:18:01 +0530

    Variables

---
+++ variables.dart
@@ -0,0 +1,13 @@
+void main() {
+  var name = "Wasath";
+  var year = 2002;
+  var height = 1.75;
+  var isMale = true;
+  var hobbies = ["Reading", "Coding", "Gaming"];
+  var address = {"street": "No-28/B", "city": "Ja-Ela", "country": "Sri Lanka"};
+  var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+  var image = {
+    'tags': ['saturn'],
+    'url': '//path/to/saturn.jpg'
+  };
+}

0c47435bc1f6a2767da13dabb63a35b073717f5b
WasathTheekshana <wasath.vt@gmail.com>
2023-08-31 19:02:53 +0530

    If else

---
+++ if-else.dart
@@ -0,0 +1,11 @@
+void main() {
+  int age = 18;
+
+  if (age >= 18) {
+    print("You are an adult!");
+  } else if (age < 18) {
+    print("You are not an adult!");
+  } else {
+    print("You are not a human!");
+  }
+}
